67bb1061f683806e73f51388bf8dc56e386d69ef
PasqM <pasqmigl97@gmail.com>
2026-03-03 12:01:15 +0100

    Reorganization

--- pipeline.sh
+++ pipeline.sh
@@ -41,19 +41,19 @@ done
 INPUT_FILE=$(basename $INPUT)
 INPUT_FOLDER=$(dirname $INPUT)
 
-python3 fasta_to_json_AF3.py \
+python3 src/fasta_to_json_AF3.py \
     --fasta_file $INPUT \
     --job_name ${INPUT_FILE%%.fasta} \
     --output_file ${INPUT%%.fasta}.json
 
 INPUT_NAME=${INPUT_FILE%%.fasta}
 
-qsub -Wblock=true -v HOME_FOLDER=$HOME_FOLDER,DATABASE_FOLDER=$DATABASE_FOLDER,INPUT_FOLDER=$INPUT_FOLDER,INPUT_FILE=$INPUT_NAME.json,OUTPUT_FOLDER=$OUTPUT_FOLDER build_alignment.sh
+qsub -Wblock=true -v HOME_FOLDER=$HOME_FOLDER,DATABASE_FOLDER=$DATABASE_FOLDER,INPUT_FOLDER=$INPUT_FOLDER,INPUT_FILE=$INPUT_NAME.json,OUTPUT_FOLDER=$OUTPUT_FOLDER utils/build_alignment.sh
 
 gprotein_list=("P29992" "Q03113" "Q14344" "O95837" "P30679" "P63096" "P04899" "P08754" "P38405" "P50148" "P63092" "P19086" "P09471")
 
 for gprotein in ${gprotein_list[@]}; do
-    python3 generate_json.py \
+    python3 src/generate_json.py \
         ${INPUT%%.fasta} \
         $ALIGNMENT_FOLDER/$gprotein \
         $ALIGNMENT_FOLDER/P62873 \
@@ -62,26 +62,26 @@ for gprotein in ${gprotein_list[@]}; do
         --name $INPUT_NAME\_$gprotein
     qsub -Wblock=true \
         -v HOME_FOLDER=$HOME_FOLDER,DATABASE_FOLDER=$DATABASE_FOLDER,INPUT_FOLDER=$INPUT_FOLDER,INPUT_FILE=$INPUT_NAME\_$gprotein.json,OUTPUT_FOLDER=$OUTPUT_FOLDER \
-        build_structure.sh
-    python3 filter.py \
+        src/build_structure.sh
+    python3 src/filter.py \
         --input_cif $OUTPUT_FOLDER/${INPUT_NAME,,}_${gprotein,,}/${INPUT_NAME,,}_${gprotein,,}_model.cif \
         --output_pdb $OUTPUT_FOLDER/${INPUT_NAME,,}_${gprotein,,}/${INPUT_NAME,,}_${gprotein,,}_filtered.pdb
-    python3 /home/pmiglionico/FoldDock/src/pdockq.py \
+    python3 src/pdockq.py \
         --pdbfile $OUTPUT_FOLDER/${INPUT_NAME,,}_${gprotein,,}/${INPUT_NAME,,}_${gprotein,,}_filtered.pdb \
         > $OUTPUT_FOLDER/${INPUT_NAME,,}_${gprotein,,}/${INPUT_NAME,,}_${gprotein,,}_pdockq.txt
 done
 
-qsub -Wblock=true -v INPUT_FOLDER=$OUTPUT_FOLDER,INPUT_NAME=${INPUT_NAME,,},OUTPUT_FOLDER=$OUTPUT_FOLDER relax.sh
-qsub -Wblock=true -v INPUT_FOLDER=$OUTPUT_FOLDER,INPUT_NAME=${INPUT_NAME,,},OUTPUT_FOLDER=$OUTPUT_FOLDER interface_energy.sh
+qsub -Wblock=true -v INPUT_FOLDER=$OUTPUT_FOLDER,INPUT_NAME=${INPUT_NAME,,},OUTPUT_FOLDER=$OUTPUT_FOLDER src/relax.sh
+qsub -Wblock=true -v INPUT_FOLDER=$OUTPUT_FOLDER,INPUT_NAME=${INPUT_NAME,,},OUTPUT_FOLDER=$OUTPUT_FOLDER src/interface_energy.sh
 blastp -query $INPUT -db GPCRDB -out $OUTPUT_FOLDER/$INPUT_NAME.out -outfmt 0
-python3 extract_plddt_json.py \
+python3 src/extract_plddt_json.py \
     --gpcr $INPUT_NAME \
     --input_dir $OUTPUT_FOLDER \
     --output_json $OUTPUT_FOLDER/${INPUT_NAME,,}_plddt.json
-python3 create_table.py \
+python3 src/create_table.py \
     --gpcr-id $INPUT_NAME \
     --plddt-json $OUTPUT_FOLDER/${INPUT_NAME,,}_plddt.json \
     --blastp-out $OUTPUT_FOLDER/$INPUT_NAME.out \
     --results-dir $OUTPUT_FOLDER \
     --output-file $OUTPUT_FOLDER/$INPUT_NAME\_table.tsv
-qsub -Wblock=true -v OUTPUT_FOLDER=$OUTPUT_FOLDER,INPUT_NAME=$INPUT_NAME,HOME_FOLDER=$PWD model.sh
+qsub -Wblock=true -v OUTPUT_FOLDER=$OUTPUT_FOLDER,INPUT_NAME=$INPUT_NAME,HOME_FOLDER=$PWD src/model.sh
